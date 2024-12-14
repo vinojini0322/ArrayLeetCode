92507217fa3d712db8f0d6dc5b837825f3ee53d7
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2024-12-14 15:44:38 +0530

    Solve Leetcode 35 - Search insert position

--- src/main/java/org/example/ArrayLeetCode.java
+++ src/main/java/org/example/ArrayLeetCode.java
@@ -46,4 +46,21 @@ public class ArrayLeetCode {
         }
         return j;
     }
+
+    //    Leetcode 35 - Search Insert Position
+    private static int searchValue(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
 }
